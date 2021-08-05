96ef63943dd2a9ec01af7261297bfcea13dc9f6c
Fahim Tajwar <tajwarfahim@gmail.com>
2021-08-05 19:34:25 +0600

    debug

--- duplicate_analysis/dedup.py
+++ duplicate_analysis/dedup.py
@@ -19,6 +19,11 @@ def parse_script_arguments():
 
 def check_duplicates_in_same_directory(target_dir):
     all_files = get_all_hdf5_files_in_a_directory(dir_path = target_dir)
+    all_files.sort()
+
+    print("\n Directory:", target_dir)
+    print("Dataset files in directory: ", all_files)
+    print("Checking if there are duplicates between any two of these files.\n")
 
     for i in range(len(all_files) - 1):
         for j in range(i + 1, len(all_files)):
